b6a8f782affd59d6bc1097ff4c671922fcb4648e
Dominik <dominik.reinartz@proton.me>
2025-01-04 12:49:04 +0100

    Set source language to en_US

--- scripts/update-langs.sh
+++ scripts/update-langs.sh
@@ -3,5 +3,5 @@
 LANG_DIR="microstation/langs"
 
 for lang_file in "$LANG_DIR"/*.ts; do
-    lupdate microstation/ -ts "$lang_file"
+    lupdate -tr-function-alias translate=tr microstation/ -ts $lang_file -no-obsolete -source-language en_US
 done
