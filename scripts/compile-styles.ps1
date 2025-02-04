e04b3c353ffff919ee93f9b426456f4af8842871
Dominik <dominik.reinartz@proton.me>
2025-02-04 16:32:18 +0100

    Removed aliases in compile-styles.ps1

--- scripts/compile-styles.ps1
+++ scripts/compile-styles.ps1
@@ -1,18 +1,18 @@
 deactivate
-mkdir style_clones
-cd style_clones
+New-Item -Fo style_clones
+Set-Location style_clones
 git clone https://github.com/Alexhuszagh/BreezeStyleSheets
-cd BreezeStyleSheets
+Set-Location BreezeStyleSheets
 python -m venv .venv
 . .venv/Scripts/Activate.ps1
 pip install PySide6
 python configure.py --styles=all --extensions=all --qt-framework pyqt6 --resource breeze.qrc --compiled-resource "breeze_pyqt6.py"
-mkdir -p ../../microstation/external_styles/breeze
+New-Item -Fo ../../microstation/external_styles/breeze
 Copy-Item -Fo LICENSE.md ../../microstation/external_styles/breeze/
 Copy-Item -R -Fo dist/* ../../microstation/external_styles/breeze/
 Copy-Item -Fo resources/breeze_pyqt6.py ../../microstation/external_styles/breeze/
 Remove-Item -Recurse -Force ../../microstation/external_styles/breeze/*-alt
 deactivate
-cd ../../
+Set-Location ../../
 Remove-Item -Recurse -Force style_clones
 . .venv/Scripts/Activate.ps1
