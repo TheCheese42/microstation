2c065ed493ff4ef9f9d6262e7c70a4317a31b02b
Dominik <dominik.reinartz@proton.me>
2025-02-04 16:27:26 +0100

    Fixed some windows scripts

--- scripts/compile-icons.ps1
+++ scripts/compile-icons.ps1
@@ -1,2 +1,2 @@
-pyrcc5 microstation/icons/icons.qrc -o microstation/icons/resource.py
+pyrcc5 microstation/icons/icons.qrc --output microstation/icons/resource.py
 (Get-Content microstation/icons/resource.py) -replace 'PyQt5', 'PyQt6' | Set-Content microstation/icons/resource.py
--- scripts/compile-styles.ps1
+++ scripts/compile-styles.ps1
@@ -8,11 +8,11 @@ python -m venv .venv
 pip install PySide6
 python configure.py --styles=all --extensions=all --qt-framework pyqt6 --resource breeze.qrc --compiled-resource "breeze_pyqt6.py"
 mkdir -p ../../microstation/external_styles/breeze
-cp -f LICENSE.md ../../microstation/external_styles/breeze/
-cp -rf dist/* ../../microstation/external_styles/breeze/
-cp -f resources/breeze_pyqt6.py ../../microstation/external_styles/breeze/
+Copy-Item -Fo LICENSE.md ../../microstation/external_styles/breeze/
+Copy-Item -R -Fo dist/* ../../microstation/external_styles/breeze/
+Copy-Item -Fo resources/breeze_pyqt6.py ../../microstation/external_styles/breeze/
 Remove-Item -Recurse -Force ../../microstation/external_styles/breeze/*-alt
 deactivate
 cd ../../
-rm -rf style_clones
+Remove-Item -Recurse -Force style_clones
 . .venv/Scripts/Activate.ps1
