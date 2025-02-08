d995173a087ea06872dee4b3c3b1d23ffacf438d
Dominik <dominik.reinartz@proton.me>
2025-02-08 18:17:57 +0100

    Fixed mypy error

--- microstation/version.py
+++ microstation/version.py
@@ -13,7 +13,7 @@ try:
 except ImportError:
     # Import fallback should stay here because the bump_version.py scripts
     # Can't handle the relative import
-    from paths import VERSION_PATH
+    from paths import VERSION_PATH  # type: ignore[no-redef]
 
 version_string = VERSION_PATH.read_text(encoding="utf-8").strip()
 __version__: tuple[int, ...] = tuple(map(int, version_string.split(".")))
